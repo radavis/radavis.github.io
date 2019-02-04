a8b5fd6a5f0efe220fedaec79f5c6d64e39cd52b
Richard Davis <rdavis.bacs@gmail.com>
2019-02-04 18:11:47 -0500

    auto-commit _drafts at

--- scripts/auto_commit.sh
+++ scripts/auto_commit.sh
@@ -1,31 +1,31 @@
 #!/usr/bin/env bash
 
 folder_checksum() {
-  echo `tar -cf - $1 | md5`
+  echo $(tar -cf - $1 | md5)
 }
 
 timestamp() {
-  echo `date "+%Y%m%d %H%M%S"`
+  echo $("date '+%Y%m%d %H%M%S'")
 }
 
-script=`basename "$0"`
+script=$(basename "$0")
 first_run=true
 pathname=$1
-initial_checksum=$(folder_checksum $pathname)
+initial_checksum=$(folder_checksum "$pathname")
 
 while true; do
-  current_checksum=$(folder_checksum $pathname)
+  current_checksum=$(folder_checksum "$pathname")
 
-  if [ $initial_checksum != $current_checksum ]; then
+  if [ "$initial_checksum" != "$current_checksum" ]; then
     current_timestamp=$(timestamp)
-    echo "$script: publishing changes at $current_timestamp"
+    echo "\n$script: publishing changes at $current_timestamp"
     git add .
     git commit -m "auto-commit $pathname at $current_timestamp"
     git push origin HEAD
     initial_checksum=$current_checksum
   fi
 
-  if [[ "$first_run" == "true" && $initial_checksum == $current_checksum ]]; then
+  if [[ "$first_run" == "true" && "$initial_checksum" == "$current_checksum" ]]; then
     echo "$script: no changes, yet..."
     first_run=false
   fi
